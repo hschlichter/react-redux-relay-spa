8d31de21829511cffeeefd037ff54a7870cff7f1
hschlichter <henrik.schlichter@gmail.com>
2016-02-05 13:07:27 +0100

    Webpack configuration tweaking.

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,6 @@ module.exports = [{
 				exclude: /node_modules/,
 				loader: 'babel-loader',
 				query: {
-					presets: ['es2015', 'stage-0', 'react'],
 					plugins: ['./babelRelayPlugin']
 				}
 			},
